b0a6675993b1515f6f2ecb8b2db16ee322eef7e7
olympiawoj <owojcik7@gmail.com>
2020-07-31 09:27:38 -0400

    Get Speech Voices

--- script.js
+++ script.js
@@ -1,77 +1,104 @@
-const main = document.querySelector('main');
-const voicesSelect = document.getElementById('voices');
-const textarea = document.getElementById('text');
-const readBtn = document.getElementById('read');
-const toggleBtn = document.getElementById('toggle');
-const closeBtn = document.getElementById('close');
-
+const main = document.querySelector("main");
+const voicesSelect = document.getElementById("voices");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toggle");
+const closeBtn = document.getElementById("close");
 
 const data = [
-    {
-      image: './img/drink.jpg',
-      text: "I'm Thirsty"
-    },
-    {
-      image: './img/food.jpg',
-      text: "I'm Hungry"
-    },
-    {
-      image: './img/tired.jpg',
-      text: "I'm Tired"
-    },
-    {
-      image: './img/hurt.jpg',
-      text: "I'm Hurt"
-    },
-    {
-      image: './img/happy.jpg',
-      text: "I'm Happy"
-    },
-    {
-      image: './img/angry.jpg',
-      text: "I'm Angry"
-    },
-    {
-      image: './img/sad.jpg',
-      text: "I'm Sad"
-    },
-    {
-      image: './img/scared.jpg',
-      text: "I'm Scared"
-    },
-    {
-      image: './img/outside.jpg',
-      text: 'I Want To Go Outside'
-    },
-    {
-      image: './img/home.jpg',
-      text: 'I Want To Go Home'
-    },
-    {
-      image: './img/school.jpg',
-      text: 'I Want To Go To School'
-    },
-    {
-      image: './img/grandma.jpg',
-      text: 'I Want To Go To Grandmas'
-    }
-  ];
+  {
+    image: "./img/drink.jpg",
+    text: "I'm Thirsty",
+  },
+  {
+    image: "./img/food.jpg",
+    text: "I'm Hungry",
+  },
+  {
+    image: "./img/tired.jpg",
+    text: "I'm Tired",
+  },
+  {
+    image: "./img/hurt.jpg",
+    text: "I'm Hurt",
+  },
+  {
+    image: "./img/happy.jpg",
+    text: "I'm Happy",
+  },
+  {
+    image: "./img/angry.jpg",
+    text: "I'm Angry",
+  },
+  {
+    image: "./img/sad.jpg",
+    text: "I'm Sad",
+  },
+  {
+    image: "./img/scared.jpg",
+    text: "I'm Scared",
+  },
+  {
+    image: "./img/outside.jpg",
+    text: "I Want To Go Outside",
+  },
+  {
+    image: "./img/home.jpg",
+    text: "I Want To Go Home",
+  },
+  {
+    image: "./img/school.jpg",
+    text: "I Want To Go To School",
+  },
+  {
+    image: "./img/grandma.jpg",
+    text: "I Want To Go To Grandmas",
+  },
+];
 
+data.forEach(createBox);
 
-  data.forEach(createBox)
+// Create speech boxes
+function createBox(item) {
+  const box = document.createElement("div");
 
-  // Create speech boxes
-  function createBox(item){
-    const box = document.createElement('div')
-    
-    const {image, text} = item //destructuring
+  const { image, text } = item; //destructuring
 
-    box.classList.add('box');
-    box.innerHTML = `
+  box.classList.add("box");
+  box.innerHTML = `
         <img src="${image}" alt=${text} />
         <p class="info">${text}</p>
     `;
-    // @todo - speak event
-    main.appendChild(box)
-  }
+  // @todo - speak event
+  main.appendChild(box);
+}
 
+// Store voices
+let voices = [];
+function getVoices(){
+  voices = speechSynthesis.getVoices();
+  voices.forEach((voice) => {
+    // build an option for each
+    const option = document.createElement('option')
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`
+
+    voicesSelect.appendChild(option)
+
+  })
+}
+
+// Voices changesd
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+// Close button
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+getVoices();
